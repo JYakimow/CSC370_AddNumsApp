e2b1d439b44bbb0a626ddaba1ed3500fc9183533
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2021-02-19 10:40:55 -0600

    Add project files.

---
+++ CSC370_Assignment4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace CSC370_Assignment4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.output_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.reset_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input_textBox
+            // 
+            this.input_textBox.Location = new System.Drawing.Point(146, 12);
+            this.input_textBox.Name = "input_textBox";
+            this.input_textBox.Size = new System.Drawing.Size(154, 20);
+            this.input_textBox.TabIndex = 0;
+            this.input_textBox.TextChanged += new System.EventHandler(this.input_textBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current Weight of Shim:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(89, 51);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(92, 32);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // output_textBox
+            // 
+            this.output_textBox.Location = new System.Drawing.Point(89, 106);
+            this.output_textBox.Name = "output_textBox";
+            this.output_textBox.Size = new System.Drawing.Size(92, 20);
+            this.output_textBox.TabIndex = 3;
+            this.output_textBox.TextChanged += new System.EventHandler(this.output_textBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Running total:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // reset_button
+            // 
+            this.reset_button.Location = new System.Drawing.Point(89, 132);
+            this.reset_button.Name = "reset_button";
+            this.reset_button.Size = new System.Drawing.Size(92, 32);
+            this.reset_button.TabIndex = 5;
+            this.reset_button.Text = "Reset";
+            this.reset_button.UseVisualStyleBackColor = true;
+            this.reset_button.Click += new System.EventHandler(this.reset_button_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.buttonAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 203);
+            this.Controls.Add(this.reset_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.output_textBox);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox output_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button reset_button;
+    }
+}
+
---
+++ CSC370_Assignment4/Form1.cs
@@ -0,0 +1,86 @@
+﻿//assignment 4 by jonathan yakimow
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSC370_Assignment4
+{
+    public partial class Form1 : Form
+    {
+        string inputString = "";
+        string outputString = "";
+        int total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //read value from textbox and assign to variable and conver to integer to do logical operations
+                inputString = this.input_textBox.Text; 
+                int input = int.Parse(inputString);
+                total = input + total;
+
+                //assign current total to to string and assign bottom textbox value
+                outputString = total.ToString();
+                this.output_textBox.Text = outputString;
+
+                //reset values to prepare for more input
+                inputString = "";
+                this.input_textBox.Text = "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                MessageBox.Show(ex.Message.ToString());
+                this.input_textBox.Text = "";
+            }   
+        }
+
+        private void output_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reset_button_Click(object sender, EventArgs e)
+        {
+            //reset all variables to being empty like they would be in a fresh start of the program
+            this.output_textBox.Text = "";
+            this.input_textBox.Text = "";
+            inputString = "";
+            outputString = "";
+            total = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
