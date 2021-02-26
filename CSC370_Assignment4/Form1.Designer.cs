5cd4e33843fbb0661a24f3bfb18629e6e020e090
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2021-02-25 22:13:08 -0600

    update

--- CSC370_Assignment4/Form1.Designer.cs
+++ CSC370_Assignment4/Form1.Designer.cs
@@ -35,11 +35,22 @@ namespace CSC370_Assignment4
             this.output_textBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.reset_button = new System.Windows.Forms.Button();
+            this.undo_buttton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.counter_textBox = new System.Windows.Forms.TextBox();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToUseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printYourResultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // input_textBox
             // 
-            this.input_textBox.Location = new System.Drawing.Point(146, 12);
+            this.input_textBox.Location = new System.Drawing.Point(52, 46);
             this.input_textBox.Name = "input_textBox";
             this.input_textBox.Size = new System.Drawing.Size(154, 20);
             this.input_textBox.TabIndex = 0;
@@ -48,7 +59,7 @@ namespace CSC370_Assignment4
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Location = new System.Drawing.Point(70, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 1;
@@ -57,7 +68,7 @@ namespace CSC370_Assignment4
             // 
             // buttonAdd
             // 
-            this.buttonAdd.Location = new System.Drawing.Point(89, 51);
+            this.buttonAdd.Location = new System.Drawing.Point(85, 88);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(92, 32);
             this.buttonAdd.TabIndex = 2;
@@ -67,8 +78,9 @@ namespace CSC370_Assignment4
             // 
             // output_textBox
             // 
-            this.output_textBox.Location = new System.Drawing.Point(89, 106);
+            this.output_textBox.Location = new System.Drawing.Point(138, 134);
             this.output_textBox.Name = "output_textBox";
+            this.output_textBox.ReadOnly = true;
             this.output_textBox.Size = new System.Drawing.Size(92, 20);
             this.output_textBox.TabIndex = 3;
             this.output_textBox.TextChanged += new System.EventHandler(this.output_textBox_TextChanged);
@@ -76,37 +88,132 @@ namespace CSC370_Assignment4
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(100, 90);
+            this.label2.Location = new System.Drawing.Point(14, 137);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(114, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Running total:";
+            this.label2.Text = "Total Weight of Shims:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // reset_button
             // 
-            this.reset_button.Location = new System.Drawing.Point(89, 132);
+            this.reset_button.Location = new System.Drawing.Point(138, 203);
             this.reset_button.Name = "reset_button";
             this.reset_button.Size = new System.Drawing.Size(92, 32);
             this.reset_button.TabIndex = 5;
-            this.reset_button.Text = "Reset";
+            this.reset_button.Text = "Reset Form";
             this.reset_button.UseVisualStyleBackColor = true;
             this.reset_button.Click += new System.EventHandler(this.reset_button_Click);
+            // 
+            // undo_buttton
+            // 
+            this.undo_buttton.Location = new System.Drawing.Point(40, 203);
+            this.undo_buttton.Name = "undo_buttton";
+            this.undo_buttton.Size = new System.Drawing.Size(92, 32);
+            this.undo_buttton.TabIndex = 6;
+            this.undo_buttton.Text = "Undo Last";
+            this.undo_buttton.UseVisualStyleBackColor = true;
+            this.undo_buttton.Click += new System.EventHandler(this.undo_buttton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Number of Shims:";
+            // 
+            // counter_textBox
+            // 
+            this.counter_textBox.Location = new System.Drawing.Point(138, 163);
+            this.counter_textBox.Name = "counter_textBox";
+            this.counter_textBox.ReadOnly = true;
+            this.counter_textBox.Size = new System.Drawing.Size(92, 20);
+            this.counter_textBox.TabIndex = 7;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(37, 71);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 9;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(248, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.howToUseToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // howToUseToolStripMenuItem
+            // 
+            this.howToUseToolStripMenuItem.Name = "howToUseToolStripMenuItem";
+            this.howToUseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.howToUseToolStripMenuItem.Text = "How to use";
+            this.howToUseToolStripMenuItem.Click += new System.EventHandler(this.howToUseToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printYourResultsToolStripMenuItem});
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // printYourResultsToolStripMenuItem
+            // 
+            this.printYourResultsToolStripMenuItem.Name = "printYourResultsToolStripMenuItem";
+            this.printYourResultsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.printYourResultsToolStripMenuItem.Text = "Print your results";
+            this.printYourResultsToolStripMenuItem.Click += new System.EventHandler(this.printYourResultsToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AcceptButton = this.buttonAdd;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 203);
+            this.ClientSize = new System.Drawing.Size(248, 250);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.counter_textBox);
+            this.Controls.Add(this.undo_buttton);
             this.Controls.Add(this.reset_button);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.output_textBox);
             this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.input_textBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Shims";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,6 +227,16 @@ namespace CSC370_Assignment4
         private System.Windows.Forms.TextBox output_textBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button reset_button;
+        private System.Windows.Forms.Button undo_buttton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox counter_textBox;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToUseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printYourResultsToolStripMenuItem;
     }
 }
 
--- CSC370_Assignment4/Form1.cs
+++ CSC370_Assignment4/Form1.cs
@@ -1,4 +1,5 @@
-﻿//assignment 4 by jonathan yakimow
+﻿//add numbers app by Jonathan Yakimow
+//for csc 370 with Dr. Locklair
 
 using System;
 using System.Collections.Generic;
@@ -16,11 +17,18 @@ namespace CSC370_Assignment4
     {
         string inputString = "";
         string outputString = "";
-        int total = 0;
+        string countOutput = "";
+        int  resetStatus = 0;
+        float total = 0;
+        float lastTotal = 0;
+        int count = 0;
+        //float TempLastInput = 0;
 
         public Form1()
         {
             InitializeComponent();
+            input_textBox.Focus();
+            resetValues();
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -44,22 +52,27 @@ namespace CSC370_Assignment4
             {
                 //read value from textbox and assign to variable and conver to integer to do logical operations
                 inputString = this.input_textBox.Text; 
-                int input = int.Parse(inputString);
+                float input = float.Parse(inputString);
+                lastTotal = total;
                 total = input + total;
+                count++;
 
                 //assign current total to to string and assign bottom textbox value
                 outputString = total.ToString();
                 this.output_textBox.Text = outputString;
+                countOutput = count.ToString();
+                this.counter_textBox.Text = countOutput;
 
                 //reset values to prepare for more input
-                inputString = "";
-                this.input_textBox.Text = "";
+                prepForInput();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message.ToString());
-                MessageBox.Show(ex.Message.ToString());
+                //MessageBox.Show(ex.Message.ToString());
+                this.errorLabel.Text = ex.Message.ToString();
                 this.input_textBox.Text = "";
+                input_textBox.Focus();
             }   
         }
 
@@ -70,17 +83,84 @@ namespace CSC370_Assignment4
 
         private void reset_button_Click(object sender, EventArgs e)
         {
-            //reset all variables to being empty like they would be in a fresh start of the program
-            this.output_textBox.Text = "";
-            this.input_textBox.Text = "";
-            inputString = "";
-            outputString = "";
-            total = 0;
+            resetValues();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void undo_buttton_Click(object sender, EventArgs e)
+        {
+            if (resetStatus == 1)
+            {
+                total = lastTotal;
+                count--;
+                countOutput = count.ToString();
+                this.counter_textBox.Text = countOutput;
+                outputString = total.ToString();
+                this.output_textBox.Text = outputString;
+                
+                inputString = "";
+                this.input_textBox.Text = "";
+                this.errorLabel.Text = "";
+                input_textBox.Focus();
+                resetStatus = 0;
+            }
+            else 
+            {
+                input_textBox.Focus();
+                this.errorLabel.Text = "";
+                this.input_textBox.Text = "";
+                inputString = "";
+                MessageBox.Show("Cannot perform undo operation");
+                //this.errorLabel.Text = "Cannot perform undo operation";
+            }
+        }
+
+        private void prepForInput()
+        {
+            //reset values to prepare for more input
+            inputString = "";
+            this.input_textBox.Text = "";
+            input_textBox.Focus();
+            resetStatus = 1;
+            this.errorLabel.Text = "";
+        }
+
+        private void resetValues()
+        {
+            //reset all variables to being empty like they would be in a fresh start of the program
+            this.output_textBox.Text = "";
+            this.input_textBox.Text = "";
+            this.counter_textBox.Text = "";
+            inputString = "";
+            outputString = "";
+            countOutput = "";
+            this.errorLabel.Text = "";
+            total = 0;
+            count = 0;
+            input_textBox.Focus();
+            resetStatus = 0;
+        }
+
+        private void howToUseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You can use this app to add the weight of multiple shims together and keep track of the total number of shims. " +
+                "Just type the value you wish to add into the top textbox and press the 'add' button. The total number of shims and the running total of all the weights" +
+                " will be added to the 'total weight' field. If you made a mistake you can undo it by pressing the 'undo' button. If you wish to reset the app you can " +
+                "press the 'reset' button.");
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void printYourResultsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Total weight of shims: " + outputString + "\n \n" + "Number of shims: " + countOutput);
+            resetValues();
         }
     }
 }
